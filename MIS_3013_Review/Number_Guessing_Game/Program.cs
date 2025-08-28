cdc3e61c6f52722aa711326939c1059c67eb8ac8
lraleman <Lucas.aleman@ou.edu>
2025-08-28 13:04:43 -0500

    changes 8.28.25

--- MIS_3013_Review/Number_Guessing_Game/Program.cs
+++ MIS_3013_Review/Number_Guessing_Game/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Runtime.InteropServices;
+
+Random r = new Random();
+int rando = r.Next(1, 5+1);
+int guess;
+int count = 0;
+do
+{
+    Console.WriteLine("Please guess a number between 1 and 5 <<");
+    string userGuess = Console.ReadLine();
+    Console.WriteLine($"You guessed {userGuess}");
+    guess = Convert.ToInt32( userGuess);
+    count++;
+    if (guess == rando)
+    {
+        
+    }
+    else if(guess > rando)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Incorrect: Too High");
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+    else
+    {
+        Console.ForegroundColor= ConsoleColor.Blue;
+        Console.WriteLine("Incorrect: Too Low");
+        Console.ForegroundColor=ConsoleColor.White;
+    }
+    //Console.WriteLine($"Generated a random number with the value {rando}");
+}
+while (guess != rando);
+Console.WriteLine($"You guessed correctly and it only took {count} attempts");
